2602f917f837006e1d0bed1b26d35add5f56248e
Sergio Nezhigay <abc1971h@gmail.com>
2023-04-08 20:00:51 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const resultEl = document.querySelector(".result");
+let task = "<h1> HomeWork #12 </h1>\n";
+
+// Лучший вариант видел и уже знаю, но сам додумался только до этого :(
+// С этим способом собес я б, наверно, не прошел ? ;)
+
+function _pow(num, degree, current) {
+  current *= num;
+  if (degree > 1) {
+    return _pow(num, degree - 1, current);
+  } else {
+    return current;
+  }
+}
+
+function pow(num, degree) {
+  if (!num) return 0;
+  if (degree === 1) return num;
+  if (degree > 0) return _pow(num, degree, 1);
+  else return 1 / _pow(num, -degree, 1);
+}
+
+task += `<p>pow(0, 3) = ${pow(0, 3)}</p>`;
+task += `<p>pow(1, 3) = ${pow(1, 3)}</p>`;
+task += `<p>pow(2, 3) = ${pow(2, 3)}</p>`;
+task += `<p>pow(3, 3) = ${pow(3, 3)}</p>`;
+
+resultEl.insertAdjacentHTML("beforeend", task);
